5624ad2b9b5ac3954178847cebb9586d675b8e55
Colby-Hall <hallcc@hendrix.edu>
2019-11-02 17:09:21 -0500

    Dependency Properties

--- DesktopContactsApp/DesktopContactsApp/Controls/ContactControl.xaml.cs
+++ DesktopContactsApp/DesktopContactsApp/Controls/ContactControl.xaml.cs
@@ -20,7 +20,39 @@ namespace DesktopContactsApp.Controls
     /// Interaction logic for ContactControl.xaml
     /// </summary>
     public partial class ContactControl : UserControl
+
+        //propdp snippet
     {
+        public Contact Contact
+        {
+            get { return (Contact)GetValue(ContactProperty); }
+            set { SetValue(ContactProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ContactProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContactProperty =
+            DependencyProperty.Register("Contact", typeof(Contact), typeof(ContactControl), new PropertyMetadata(new Contact() { Name = "Name", Phone = "Number", Email = "Email" }, SetText));
+
+        private static void SetText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // allows access to all of the text blocks in the contact control
+            ContactControl control = d as ContactControl;
+
+            if (control != null)
+            {
+                control.nameTextBlock.Text = (e.NewValue as Contact).Name;
+                control.emailTextBlock.Text = (e.NewValue as Contact).Email;
+                control.phoneTextBlock.Text = (e.NewValue as Contact).Phone;
+            }
+        }
+
+        // A callback is a method that is executed every time the property changes
+
+
+
+
+        /* Replacing with a dependency property
+    
         private Contact contact;
 
         public Contact Contact
@@ -34,6 +66,7 @@ namespace DesktopContactsApp.Controls
                 phoneTextBlock.Text = contact.Phone;
             }
         }
+        */
 
 
         public ContactControl()
